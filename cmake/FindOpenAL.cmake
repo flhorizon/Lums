3a162a168c8c58bad2a24d7b6e6818df1c4cc39e
Florian Fouratier <ffourati@student.42.fr>
2015-08-23 01:35:12 +0200

    Can CMAKE on Windows, but build targets are missing x64

---
+++ cmake/FindOpenAL.cmake
@@ -0,0 +1,55 @@
+#
+# FindOpenAL.cmake
+#
+
+if (WIN32)
+
+	set(WIN_LIB_PATH ${CMAKE_SOURCE_DIR}/ext)
+	if (MSVC)
+		set(WIN_LIB_PATH "${WIN_LIB_PATH}/msvc")
+	else()
+		set(WIN_LIB_PATH "${WIN_LIB_PATH}/mingw")
+	endif()
+	if (${CMAKE_SIZEOF_VOID_P} EQUAL 8)
+		set (WIN_LIB_PATH "${WIN_LIB_PATH}64")
+	endif()
+
+	if (CMAKE_BUILD_TYPE MATCHES "Debug")
+		set (WIN_DEBUG "d")
+	endif()
+
+	set (OPENAL_LIBRARIES "${WIN_LIB_PATH}/lib/OpenAL32${WIN_DEBUG}.lib")
+	set (OPENAL_INCLUDE_DIRS "${WIN_LIB_PATH}/include/OpenAL")
+
+else()
+
+	find_path(OPENAL_INCLUDE_DIR al.h
+		HINTS
+		ENV OPENALDIR
+		PATH_SUFFIXES include/AL include/OpenAL include
+		PATHS
+		~/Library/Frameworks
+		/Library/Frameworks
+		/sw # Fink
+		/opt/local # DarwinPorts
+		/opt/csw # Blastwave
+		/opt
+	)
+	
+	find_library(OPENAL_LIBRARY
+		NAMES OpenAL al openal OpenAL32
+		HINTS
+		ENV OPENALDIR
+		PATH_SUFFIXES lib64 lib libs64 libs libs/Win32 libs/Win64
+		PATHS
+		~/Library/Frameworks
+		/Library/Frameworks
+		/sw
+		/opt/local
+		/opt/csw
+		/opt
+	)
+
+endif()
+
+message(STATUS "Found OpenAL: ${OPENAL_LIBRARIES}")
--- cmake/FindVorbis.cmake
+++ cmake/FindVorbis.cmake
@@ -2,45 +2,75 @@
 # FindVorbis.cmake
 #
 
-set(VORBIS_SEARCH_PATH
-	/usr
-	/usr/local
-	~/.brew
-)
+if (WIN32)
 
-find_library(
-	VORBIS_LIBRARIES
-	vorbis
-	PATHS
-	${VORBIS_SEARCH_PATH}
-	PATH_SUFFIXES lib lib64
-)
+	set(WIN_LIB_PATH ${CMAKE_SOURCE_DIR}/ext)
+	if (MSVC)
+		set(WIN_LIB_PATH "${WIN_LIB_PATH}/msvc")
+	else()
+		set(WIN_LIB_PATH "${WIN_LIB_PATH}/mingw")
+	endif()
+	if (${CMAKE_SIZEOF_VOID_P} EQUAL 8)
+		set (WIN_LIB_PATH "${WIN_LIB_PATH}64")
+	endif()
 
-find_library(
-	OGG_LIBRARIES
-	ogg
-	PATHS
-	${VORBIS_SEARCH_PATH}
-	PATH_SUFFIXES lib lib64
-)
+	if (CMAKE_BUILD_TYPE MATCHES "Debug")
+		set (WIN_DEBUG "d")
+	endif()
 
-find_library(
-	VORBISFILE_LIBRARIES
-	vorbisfile
-	PATHS
-	${VORBIS_SEARCH_PATH}
-	PATH_SUFFIXES lib lib64
-)
+# So lame... But it works.
+	set (VORBIS_INCLUDE_DIRS "${WIN_LIB_PATH}/include")
+	set (OGG_LIBRARIES "${WIN_LIB_PATH}/lib/libogg${WIN_DEBUG}.lib")
+	set (OGG_RUNTIME "${WIN_LIB_PATH}/bin/libogg${WIN_DEBUG}.dll")
+	set (VORBISFILE_LIBRARIES "${WIN_LIB_PATH}/lib/libvorbisfile${WIN_DEBUG}.lib")
+	set (VORBISFILE_RUNTIME "${WIN_LIB_PATH}/bin/libvorbisfile${WIN_DEBUG}.dll")
+	set (VORBIS_LIBRARIES "${WIN_LIB_PATH}/lib/libvorbis${WIN_DEBUG}.lib")
+	set (VORBIS_RUNTIME "${WIN_LIB_PATH}/bin/libvorbis${WIN_DEBUG}.dll")
+	
+else()
 
-find_path(
- 	VORBIS_INCLUDE_DIRS
- 	vorbis/vorbisfile.h
- 	PATHS
- 	${VORBIS_SEARCH_PATH}
- 	PATH_SUFFIXES include
- 	NO_DEFAULT_PATH
-)
+	set(VORBIS_SEARCH_PATH
+		/usr
+		/usr/local
+		~/.brew
+	)
+	
+	find_library(
+		VORBIS_LIBRARIES
+		vorbis
+		PATHS
+		${VORBIS_SEARCH_PATH}
+		PATH_SUFFIXES lib lib64
+	)
+	
+	find_library(
+		OGG_LIBRARIES
+		ogg
+		PATHS
+		${VORBIS_SEARCH_PATH}
+		PATH_SUFFIXES lib lib64
+	)
+	
+	find_library(
+		VORBISFILE_LIBRARIES
+		vorbisfile
+		PATHS
+		${VORBIS_SEARCH_PATH}
+		PATH_SUFFIXES lib lib64
+	)
+	
+	find_path(
+		VORBIS_INCLUDE_DIRS
+		vorbis/vorbisfile.h
+		PATHS
+		${VORBIS_SEARCH_PATH}
+		PATH_SUFFIXES include
+		NO_DEFAULT_PATH
+	)
+	
+endif()
 
 list(APPEND VORBIS_LIBRARIES ${OGG_LIBRARIES} ${VORBISFILE_LIBRARIES})
+list(APPEND VORBIS_INCLUDE_DIRS ${OGG_INCLUDE_DIRS})
 
 message(STATUS "Found vorbis: ${VORBIS_LIBRARIES}")
