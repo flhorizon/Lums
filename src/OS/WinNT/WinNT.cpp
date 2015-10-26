ba48f147812ef9298d7cdc4fbb39743b5b3dd148
Florian Fouratier <flhorizon@users.noreply.github.com>
2015-10-26 13:24:42 +0100

    [WinNT] fixed client filesystem access abstraction

--- src/OS/WinNT/WinNT.cpp
+++ src/OS/WinNT/WinNT.cpp
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                                            */
-/*    WinNT/WinNT.cpp                            oooooo       oooooo      */
+/*    WinNT/WinNT.cpp                                oooooo       oooooo      */
 /*                                                 oooooooooo   oooooooooo    */
 /*                                                         o%%%%%o            */
 /*                                                         %:::::%            */
@@ -11,12 +11,13 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#define WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN 1
 #include <windows.h>
 #include "AtlConv.h"
 #include "AtlBase.h"
 #include "Shlobj.h"
 #include <LumsInclude/OperatingSystem.hpp>
+
 
 // Store UTF-8 for proper std:: C++ and convert back to UTF-16 on WIN32 calls.
 static std::string  res_path;
@@ -50,13 +51,12 @@ namespace lm
         {
             wchar_t 		wcPath[MAX_PATH];
 			std::wstring	wcPathStr;
-            HMODULE hModule = GetModuleHandle(NULL);
 
-            GetModuleFileNameW(hModule, wcPath, MAX_PATH);
+            GetModuleFileNameW(nullptr, wcPath, MAX_PATH);
 			
-			wcPathStr = wcPath;
-            wcPathStr = wcPathStr.substr(0, wcPathStr.find_last_of(L'\\') + 1);
-            normalizePath<std::wstring>(wcPathStr);
+			wcPathStr = std::wstring(wcPath);
+			normalizePath<std::wstring>(wcPathStr);
+            wcPathStr = wcPathStr.substr(0, wcPathStr.find_last_of(L'/') + 1) + L"resources";
 			
 			// Convenient ATL conversion macro.
 			// The API conversion function `WideCharToMultiByte' 
@@ -69,35 +69,31 @@ namespace lm
     }
 	
 	// Returns nullptr if the application path has not been set.
-	const std::string
-    appSupportPath()
+	const std::string&
+    userDataPath()
 	{
 		return appsupport_path;
 	}
 	
-	static BOOL
+	static bool
 	directoryExists(LPCWSTR cwPath)
 	{
 		DWORD dwAttrib = GetFileAttributesW(cwPath);
 		
 		return (dwAttrib != INVALID_FILE_ATTRIBUTES && 
-				(dwAttrib & FILE_ATTRIBUTE_DIRECTORY));
+				(dwAttrib & FILE_ATTRIBUTE_DIRECTORY) == FILE_ATTRIBUTE_DIRECTORY);
 	}
 	
 	// Returns 0 on success, as proper POSIX calls
 	// and contrary to most WIN32 API calls.
 	// Returns non-zero on error.
 	int
-	mkAppDataDir(const char *app)
+	mkAppDataDir(const char* appDir)
 	{
 		USES_CONVERSION;
-		std::wstring		dirPath(CA2W(app, CP_UTF8));
+		std::wstring		wsAppDir(CA2W(appDir, CP_UTF8));
 		PWSTR            	oPath;
 		HRESULT 			res;
-
-		// Nothing to do; success.
-		if (directoryExists(dirPath.c_str()))
-			return 0;
 
 		res = SHGetKnownFolderPath(
 			FOLDERID_RoamingAppData				// Query user's AppData\Roaming folder
@@ -108,21 +104,24 @@ namespace lm
 
 		if (res != S_OK)
 		{
-			perror("SHGetKnownFolderPath");
 			return -1;
 		}
 
-		dirPath = std::wstring(oPath) + std::wstring(L"\\") + dirPath;
+		wsAppDir = std::wstring(oPath) + std::wstring(L"\\") + wsAppDir;
 		CoTaskMemFree(oPath);
+        oPath = nullptr;
+		
+		appsupport_path = CW2A(wsAppDir.c_str(), CP_UTF8);        
+        normalizePath(appsupport_path);		
 
-        normalizePath<std::wstring>(dirPath);
-		appsupport_path = CW2A(dirPath.c_str(), CP_UTF8);
+        // Nothing to do; success.
+        if (directoryExists(wsAppDir.c_str()))
+            return 0;
 
 		// This one returns 0 on error. RLY.
-		if (CreateDirectoryW(dirPath.c_str(), nullptr) == 0)
+		if (0 == CreateDirectoryW(wsAppDir.c_str(), nullptr))
 		{
-			perror("CreateDirectoryW");
-			return -1;
+			return -2;
 		}
 
 		return 0;
