18bc87a433c0046fde6a3b024b3e851cf82369e9
Florian Fouratier <flhorizon@users.noreply.github.com>
2015-10-28 16:12:09 +0100

    [WinNT] Introduced PDB handling for Debug builds

---
+++ cmake/LmCopyIfExistsDifferent.cmake
@@ -0,0 +1,10 @@
+
+# Wrapper for the cmake -E copy_if_different.
+# Checks if the copy-source file exists.
+# Module executed as a custom command.
+
+if(EXISTS ${SOURCE_FILE})
+  execute_process(COMMAND ${CMAKE_COMMAND}
+    -E copy ${SOURCE_FILE} ${DESTINATION_FILE}
+)
+endif()
